3b50173b5fe07e46f6c9861f574ff1d46351400c
Sakibur Rahman <122334294+Sakib70708@users.noreply.github.com>
2024-07-17 20:01:49 +0600

    Add files via upload

---
+++ 6.if-elif.py
@@ -0,0 +1,13 @@
+marks=int(input("Enter your marks"))
+if marks>=80:
+    print("you got A+")
+elif marks>=70:
+    print("you got A")
+elif marks>=60:
+    print("you got A-")
+elif marks>=50:
+    print("you got B")
+elif marks>=40:
+    print("you got c")
+else:
+    print("you are fail in this subject")
